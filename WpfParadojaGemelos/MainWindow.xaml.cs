5c981fd5e1f332cf9c53bef38e8d11a6dfbe8838
Andrés Cortés <65383172+andresfcp@users.noreply.github.com>
2022-05-31 10:38:42 -0500

    Revert "Merge pull request #1 from andresfcp/getSliderDouble"
    This reverts commit 37972612bc382386daf9414a7f2f21d9d149275c, reversing
    changes made to 5c33121f05e90b529f2104335a1139fa0529b3ae.

--- WpfParadojaGemelos/MainWindow.xaml.cs
+++ WpfParadojaGemelos/MainWindow.xaml.cs
@@ -30,25 +30,15 @@ namespace WpfParadojaGemelos
 
         private void btnCalcular_Click(object sender, RoutedEventArgs e)
         {
-            calcular();
-            
-        }
+            double tViajero = double.Parse(txtTiempo.Text);
+            double porcentajeC = double.Parse(txtVelocidad.Text,CultureInfo.InvariantCulture);
 
-
-
-        private void calcular()
-        {
-            //double tViajero = double.Parse(txtTiempo.Text);
-            //double porcentajeC = double.Parse(txtVelocidad.Text,CultureInfo.InvariantCulture);
-
-            double tViajero = sldTiempoV.Value;
-            double porcentajeC = sldPorcentajeC.Value;
-
-            double tObservador = tViajero / (Math.Sqrt(1 - (Math.Pow(porcentajeC, 2) / 10000)));
+            double tObservador = tViajero / (Math.Sqrt(1 - (Math.Pow(porcentajeC,2) / 10000)));
 
             lblTiempo2.Visibility = Visibility.Visible;
             lblResultado.Content = tObservador.ToString();
         }
+
     }
 
 }
