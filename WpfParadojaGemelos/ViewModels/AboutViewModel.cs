48f2b937c039d6608c011521bc748127c2b1921c
Andrés Cortés <65383172+andresfcp@users.noreply.github.com>
2025-12-10 23:45:20 -0500

    refactor: comienza la migracion al viewmodel de about

---
+++ WpfParadojaGemelos/ViewModels/AboutViewModel.cs
@@ -0,0 +1,22 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfParadojaGemelos.ViewModels
+{
+    public partial class AboutViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        string version = "1.0.1b";
+
+        [ObservableProperty]
+        private string url = "https://acortes.co";
+
+    }
+}
