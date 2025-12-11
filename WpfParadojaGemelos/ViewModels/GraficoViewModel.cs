e95dc05c9c6f147b077bcb84873a648407dfc88c
Andrés Cortés <65383172+andresfcp@users.noreply.github.com>
2025-12-11 11:41:32 -0500

    refactor: Se mueve la lógica del grafico al viewmodel

---
+++ WpfParadojaGemelos/ViewModels/GraficoViewModel.cs
@@ -0,0 +1,64 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
+using System.Collections.ObjectModel;
+using System.Linq;
+using WpfParadojaGemelos.Models;
+
+namespace WpfParadojaGemelos.ViewModels
+{
+    public partial class GraficoViewModel : ObservableObject
+    {
+        private PlotModel plotModel;
+        public PlotModel PlotModel
+        {
+            get => plotModel;
+            private set => SetProperty(ref plotModel, value);
+        }
+
+        public GraficoViewModel(ObservableCollection<Dato> valores, int tiempoViajero)
+        {
+            BuildPlotModel(valores, tiempoViajero);
+        }
+
+        private void BuildPlotModel(ObservableCollection<Dato> valores, int tiempoViajero)
+        {
+            if (valores == null)
+            {
+                PlotModel = new PlotModel { Title = "No data" };
+                return;
+            }
+
+            var ordered = new ObservableCollection<Dato>(valores.OrderBy(x => x.Porcentaje_C));
+
+            var model = new PlotModel { Title = "Tiempo Observador vs. % Vel Luz" };
+
+            var ejeX = new LinearAxis
+            {
+                MajorGridlineStyle = LineStyle.Solid,
+                MinorGridlineStyle = LineStyle.Dot,
+                Title = "T. Observador"
+            };
+            model.Axes.Add(ejeX);
+
+            var ejeY = new LinearAxis
+            {
+                Position = AxisPosition.Bottom,
+                Title = "%C"
+            };
+            model.Axes.Add(ejeY);
+
+            var linea = new LineSeries();
+            foreach (var dato in ordered)
+            {
+                linea.Points.Add(new DataPoint(dato.Porcentaje_C, dato.Tiempo_Observador));
+            }
+            linea.Title = "Tiempo Viajero " + tiempoViajero;
+            linea.CanTrackerInterpolatePoints = false;
+            model.Series.Add(linea);
+
+            PlotModel = model;
+        }
+    }
+}
--- WpfParadojaGemelos/Views/Grafico.xaml.cs
+++ WpfParadojaGemelos/Views/Grafico.xaml.cs
@@ -1,10 +1,7 @@
-﻿using OxyPlot;
-using OxyPlot.Axes;
-using OxyPlot.Series;
-using System.Collections.ObjectModel;
-using System.Linq;
+﻿using System.Collections.ObjectModel;
 using System.Windows;
 using WpfParadojaGemelos.Models;
+using WpfParadojaGemelos.ViewModels;
 
 namespace WpfParadojaGemelos
 {
@@ -16,50 +13,7 @@ namespace WpfParadojaGemelos
         public Grafico(ObservableCollection<Dato> valores, int tiempoV)
         {
             InitializeComponent();
-            //DGDatos1.ItemsSource = valores;
-            Graficar(valores, tiempoV);
+            DataContext = new GraficoViewModel(valores, tiempoV);
         }
-
-
-        private void Graficar(ObservableCollection<Dato> valores, int tiempoViajero)
-        {
-            /*List<Dato> datos = new List<Dato>();
-            datos = valores.ToList();
-            datos.Sort(delegate (Dato x, Dato y)
-            {
-                return x.Porcentaje_C.CompareTo(y.Porcentaje_C);
-            });*/
-
-            //List<Dato> datos = valores.OrderBy(x => x.Porcentaje_C).ToList();
-            //Hasta aquí asignamos el ObservableCollection a una lista para poderlo ordenar
-            valores = new ObservableCollection<Dato>(valores.OrderBy(x => x.Porcentaje_C));
-
-            var plotModel1 = new PlotModel();
-            plotModel1.Title = "Tiempo Observador vs. % Vel Luz";
-
-            var ejeX = new LinearAxis();
-            ejeX.MajorGridlineStyle = LineStyle.Solid;
-            ejeX.MinorGridlineStyle = LineStyle.Dot;
-            ejeX.Title = "T. Observador";
-            //ejeX.Minimum = 0;
-            plotModel1.Axes.Add(ejeX);
-
-            var ejeY = new LinearAxis();
-            ejeY.Position = AxisPosition.Bottom;
-            ejeY.Title = "%C";
-            plotModel1.Axes.Add(ejeY);
-            //Hasta aqui todo es común, ahora es asociar (el ObservableCollection) la lista con el gráfico
-
-            var linea = new LineSeries();
-            foreach (var dato in valores)
-            {
-                linea.Points.Add(new DataPoint(dato.Porcentaje_C, dato.Tiempo_Observador));
-            }
-            linea.Title = "Tiempo Viajero " + tiempoViajero;
-            linea.CanTrackerInterpolatePoints = false;
-            plotModel1.Series.Add(linea);
-            Grafica.Model = plotModel1;
-        }
-
     }
 }
